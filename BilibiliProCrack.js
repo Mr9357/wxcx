330482a1bc9a9b396ac534d17ce50facbc965f7e
Mr9357 <89758772+Mr9357@users.noreply.github.com>
2024-01-14 12:05:18 +0800

    更新 BilibiliProCrack.js

--- BilibiliProCrack.js
+++ BilibiliProCrack.js
@@ -31,15 +31,15 @@ hostname=app.bilibili.com, grpc.biliapi.net,*.biliapi.net,app.bilibili.com,api.b
 
 
 var modifiedHeaders = $request['headers'];
-modifiedHeaders['Cookie'] = '';
-modifiedHeaders['x-bili-device-bin'] = '';
-modifiedHeaders['Authorization'] = '';
-modifiedHeaders['User-Agent'] = '';
-modifiedHeaders['buvid'] = '';
-modifiedHeaders['x-bili-metadata-bin'] = '';
-modifiedHeaders['x-bili-locale-bin'] = '';
-modifiedHeaders['x-bili-network-bin'] = '';
-modifiedHeaders['x-bili-fawkes-req-bin'] = '';
-modifiedHeaders['x-bili-trace-id'] = '';
+modifiedHeaders['Cookie'] = '_uuid=A66A814E-C77B-D94B-2D4B-334C16A36F3126436infoc; buvid3=365DFB89-F45F-4CF6-81B4-E69BFA98AB6D49136infoc; b_nut=1705203027; buvid4=F97E990E-6364-E659-3B8A-0A72C1642C2027163-124011403-Nb%2BFKdit7lM8nvnvfMSiFA%3D%3D; Buvid=Y646F56E95C9BC2C47B9AE9D2A6E96DEA078; SESSDATA=45decb15%2C1720582843%2Cbf09f811CjB8uqmxUpbS9166-izdsh_wS-EKCT2oht4NUJNtEnww6YPrRE4-ImRlVUiHMWVhmboSVjBZUlpjSzFHdUVDREtObzBBZENOMzV4S1pGOV9tSklCODFzTHBreWFpbkJKOG5FbWNCWEdWZ01CVFl2Z3pkdmRDZ2dsU3hkYjl6ZU02dmZ3eEhxZDhnIIEC; DedeUserID=1545829; DedeUserID__ckMd5=200cc14ea12e6816; bili_jct=44bca603838e729cc759be95eadfc8b3; sid=p8wl11n5; buvid_fp=2272f02d7550fcefbfad7632779b200e';
+modifiedHeaders['x-bili-device-bin'] = 'CAEQhK+MJBokWTY0NkY1NkU5NUM5QkMyQzQ3QjlBRTlEMkE2RTk2REVBMDc4IgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoNaVBob25lIDcgUGx1c1IGMTUuMC4yagY3LjU3LjByQDY0MUUwOUE3MERCOTQzNzU1MUJDMTA1MzhGQjMzNDU2MjAxOTEyMjYyMTU2NTlDQzk4MUUzMDc5NzEzNjZFNkR49M2b38Ex';
+modifiedHeaders['Authorization'] = 'identify_v1 6dcd144522bcfbda39b92e1e2b3c7111CjB8uqmxUpbS9166-izdsh_wS-EKCT2oht4NUJNtEnww6YPrRE4-ImRlVUiHMWVhmboSVjBZUlpjSzFHdUVDREtObzBBZENOMzV4S1pGOV9tSklCODFzTHBreWFpbkJKOG5FbWNCWEdWZ01CVFl2Z3pkdmRDZ2dsU3hkYjl6ZU02dmZ3eEhxZDhnIIEC';
+modifiedHeaders['User-Agent'] = 'bili-universal/75700100 os/ios model/iPhone 7 Plus mobi_app/iphone osVer/15.0.2 network/2';
+modifiedHeaders['buvid'] = ''Y646F56E95C9BC2C47B9AE9D2A6E96DEA078;
+modifiedHeaders['x-bili-metadata-bin'] = 'CtwBNmRjZDE0NDUyMmJjZmJkYTM5YjkyZTFlMmIzYzcxMTFDakI4dXFteFVwYlM5MTY2LWl6ZHNoX3dTLUVLQ1Qyb2h0NE5VSk50RW53dzZZUHJSRTQtSW1SbFZVaUhNV1ZobWJvU1ZqQlpVbHBqU3pGSGRVVkRSRXRPYnpCQlpFTk9NelY0UzFwR09WOXRTa2xDT0RGelRIQnJlV0ZwYmtKS09HNUZiV05DV0VkV1owMUNWRmwyWjNwa2RtUkRaMmRzVTNoa1lqbDZaVTAyZG1aM2VFaHhaRGhuSUlFQxIGaXBob25lGgVwaG9uZSCEr4wkKgVhcHBsZTIkWTY0NkY1NkU5NUM5QkMyQzQ3QjlBRTlEMkE2RTk2REVBMDc4OgNpb3M=';
+modifiedHeaders['x-bili-locale-bin'] = 'Eg4KAnpoEgRIYW5zGgJDTg==';
+modifiedHeaders['x-bili-network-bin'] = 'CAE=';
+modifiedHeaders['x-bili-fawkes-req-bin'] = 'CgZpcGhvbmUSBHByb2QaCGMyZmM0OWFj';
+modifiedHeaders['x-bili-trace-id'] = '718d0458aceb81421e0695b33d65a359:1e0695b33d65a359:0:0';
 modifiedHeaders['x-bili-exps-bin'] = '';
 $done({'headers': modifiedHeaders});
